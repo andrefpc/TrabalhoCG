b9e6fc7a33f9d1103c5c7c51b49d354bfe841673
Jonatas Correa Barbosa <jonatascb@gmail.com>
2013-04-12 19:25:05 -0300

    adicao da funcao de pular

---
+++ include/Player.h
@@ -0,0 +1,15 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+
+
+class Player : public Camera
+{
+    public:
+        Player();
+        virtual ~Player();
+    protected:
+    private:
+};
+
+#endif // PLAYER_H
